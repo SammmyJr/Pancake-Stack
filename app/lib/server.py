098955635e0afdac754969730bc694aa9d0123af
Samuel <sammmyjr@hey.com>
2026-09-22 16:38:31 +1000

    Added functionality to reply in servers, not just DMs.

--- app/lib/server.py
+++ app/lib/server.py
@@ -6,12 +6,18 @@ from .model import chat
 
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
+owner_id = os.getenv("USER_ID")
+
+if owner_id:
+    owner_id = int(owner_id)
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.dm_messages = True
 
 bot = commands.Bot(intents=intents, command_prefix="!")
+
+keyword = "kupo"
 
 
 # runs on client connect
@@ -30,18 +36,28 @@ async def on_message(message):
 
     # message.guild is None for DMs
     if message.guild is None:
-        statusMessage = await message.channel.send("⏳ Thinking...")
+        await respond(message)
+    else:
+        # check if the author is the bot owner, don't want to reply to other people!
+        if message.author.id == owner_id:
+            # check if the author has invoked the model with a keywords
+            if message.content.lower().startswith(keyword):
+                await respond(message)
 
-        # get response from model
-        response = await chat(message.content, statusMessage)
-        if response:
-            # delete status and tool call messages, send the model's response
-            await statusMessage.delete()
-            await message.channel.send(response)
+
+async def respond(message: discord.Message):
+    statusMessage = await message.reply("⏳ Thinking...")
+
+    # get response from model
+    response = await chat(message.content, statusMessage)
+    if response:
+        # delete status and tool call messages, send the model's response
+        await statusMessage.delete()
+        await message.channel.send(response)
 
 
 def run():
-    if token:
+    if token and owner_id:
         bot.run(token)
     else:
-        print("Missing 'DISCORD_TOKEN' in .env!")
+        print("Missing 'DISCORD_TOKEN' or 'USER_ID' in .env!")
